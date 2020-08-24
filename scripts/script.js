5b4e84580ee94c392392e9729ca3a60f8585e0a2
patrickminero <58296867+mineropatricio@users.noreply.github.com>
2020-08-24 20:46:05 +0200

    upload new portfolio site
    New version of portfolio site

---
+++ scripts/script.js
@@ -0,0 +1,181 @@
+
+$('#menu-button').click(function(){
+    $('#nav-bar').slideToggle('slow');
+    $('#top').toggleClass('open1')
+    $('#middle').toggleClass('open2')
+    $('#bottom').toggleClass('open3')
+    //$('menu-button').toggleClass('red')
+})
+
+$('document').ready(function(){
+    $('#blur').fadeIn(1500)
+    englishText();
+})
+
+function englishText(){
+    $('#home-button a').text(english.header.listItems[0]);
+    $('#about-button a').text(english.header.listItems[1]);
+    $('#projects-button a').text(english.header.listItems[2]);
+    $('#contact-button a').text(english.header.listItems[3]);
+    $('#nav-bar h3').html(english.header.listItems[4])
+
+    $('#blur div h2').text(english.main.mainTitle);
+
+    $("#description-div h2").text(english.about.title)
+    $("#para1").text(english.about.paragraph1)
+    $("#para2").text(english.about.paragraph2)
+    $("#para3").text(english.about.paragraph3)
+    $('#journey').text(english.about.journey)
+    $("#para4").html(english.about.paragraph4)
+    $("#para5").html(english.about.paragraph5)
+
+    $('#projects h2').text(english.projects.title)
+    $('#project1 h2').text(english.projects.projectOne.title)
+    $('#project1 img').attr('src', english.projects.projectOne.image)
+    $('#project1 a').attr('href', english.projects.projectOne.url)
+    $('#project1 img').attr('alt', english.projects.projectOne.alt)
+    $('#project1 p').text(english.projects.projectOne.description)
+
+    $('#project2 h2').text(english.projects.projectTwo.title)
+    $('#project2 img').attr('src', english.projects.projectTwo.image)
+    $('#project2 a').attr('href', english.projects.projectTwo.url)
+    $('#project2 img').attr('alt', english.projects.projectTwo.alt)
+    $('#project2 p').text(english.projects.projectTwo.description)
+
+    $('#project3 h2').text(english.projects.projectThree.title)
+    $('#project3 img').attr('src', english.projects.projectThree.image)
+    $('#project3 a').attr('href', english.projects.projectThree.url)
+    $('#project3 img').attr('alt', english.projects.projectThree.alt)
+    $('#project3 p').text(english.projects.projectThree.description)
+
+    $('#references h2').text(english.references.title)
+    $("#reference1 p").text(english.references.initial)
+    $("#reference2 p").text(english.references.equator)
+    $("#reference3 p").text(english.references.beerworks)
+
+    $('#contact h2').text(english.contact.title)
+    $('#name').attr('placeholder', english.contact.name)
+    $('#email').attr('placeholder', english.contact.email)
+    $('#message').attr('placeholder', english.contact.message)
+    $('#send').attr('value', english.contact.submit)
+    $('#contact h2').text(english.contact.title)
+
+    $('#social-media h2').text(english.contact.footer.h2)
+    $('#social-media h3').html(english.contact.footer.h3)
+
+}
+
+function spanishText(){
+    $('#home-button a').text(spanish.header.listItems[0]);
+    $('#about-button a').text(spanish.header.listItems[1]);
+    $('#projects-button a').text(spanish.header.listItems[2]);
+    $('#contact-button a').text(spanish.header.listItems[3]);
+    $('#nav-bar h3').html(spanish.header.listItems[4])
+
+    $('#blur div h2').text(spanish.main.mainTitle);
+
+    $("#description-div h2").text(spanish.about.title)
+    $("#para1").text(spanish.about.paragraph1)
+    $("#para2").text(spanish.about.paragraph2)
+    $("#para3").text(spanish.about.paragraph3)
+    $("#para4").html(spanish.about.paragraph4)
+    $("#para5").html(spanish.about.paragraph5)
+
+    $('#projects h2').text(spanish.projects.title)
+    $('#project1 h2').text(spanish.projects.projectOne.title)
+    $('#project1 img').attr('src', spanish.projects.projectOne.image)
+    $('#project1 a').attr('href', spanish.projects.projectOne.url)
+    $('#project1 img').attr('alt', spanish.projects.projectOne.alt)
+    $('#project1 p').text(spanish.projects.projectOne.description)
+
+    $('#project2 h2').text(spanish.projects.projectTwo.title)
+    $('#project2 img').attr('src', spanish.projects.projectTwo.image)
+    $('#project2 a').attr('href', spanish.projects.projectTwo.url)
+    $('#project2 img').attr('alt', spanish.projects.projectTwo.alt)
+    $('#project2 p').text(spanish.projects.projectTwo.description)
+
+    $('#project3 h2').text(spanish.projects.projectThree.title)
+    $('#project3 img').attr('src', spanish.projects.projectThree.image)
+    $('#project3 a').attr('href', spanish.projects.projectThree.url)
+    $('#project3 img').attr('alt', spanish.projects.projectThree.alt)
+    $('#project3 p').text(spanish.projects.projectThree.description)
+
+    $('#references h2').text(spanish.references.title)
+    $("#reference1").text(spanish.references.equator)
+    $("#reference2").text(spanish.references.beerworks)
+
+    $('#contact h2').text(spanish.contact.title)
+    $('#name').attr('placeholder', spanish.contact.name)
+    $('#email').attr('placeholder', spanish.contact.email)
+    $('#message').attr('placeholder', spanish.contact.message)
+    $('#send').attr('value', spanish.contact.submit)
+    $('#contact h2').text(spanish.contact.title)
+
+    $('#social-media h3').text(spanish.contact.footer.h3)
+
+}
+
+$('#spanish').click(function(){
+    spanishText();
+})
+$('#english').click(function(){
+    englishText();
+})
+
+$('#show-more').click(function(){
+    if($('#show-more').text() === 'Show More'){
+        $('#references').height('auto')
+        $('#show-more').text('Show Less')
+        $('#show-more').css('float', 'left');
+        $('#reference1').hide();
+        $("#reference2").show();        
+        $("#reference3").show();        
+    }else{
+        $('#references').height('auto')
+        $('#show-more').text('Show More')
+        $('#show-more').css('float', 'right');
+        $('#reference1').show();
+        $("#reference1 p").text(english.references.initial);
+        $("#reference2").hide();        
+        $("#reference3").hide();
+    }
+})
+
+$('#project1').click(function(){
+    $('#project1 div').slideToggle(700)
+})
+$('#project2').click(function(){
+    $('#project2 div').slideToggle(700)
+})
+$('#project3').click(function(){
+    $('#project3 div').slideToggle(700)
+});
+
+let controller =  new ScrollMagic.Controller();
+
+let scene = new $.ScrollMagic.Scene({
+    triggerElement: '#about'
+}).setClassToggle('.portrait', 'show').addTo(controller);
+
+let scene2 = new $.ScrollMagic.Scene({
+    triggerElement: '#para1',
+    offset: 100
+}).setClassToggle('.portrait', 'move').addTo(controller);
+
+let scene3 = new $.ScrollMagic.Scene({
+    triggerElement: '#para1',
+}).setClassToggle('.description', 'to-right').addTo(controller);
+
+let scene4 = new $.ScrollMagic.Scene({
+    triggerElement: '#para1',
+    offset: 100
+}).setClassToggle('.portrait', 'side').addTo(controller);
+
+let scene5 = new $.ScrollMagic.Scene({
+    triggerElement: '#para1',
+    offset: 100
+}).setClassToggle('.description', 'inline').addTo(controller);
+
+let scene6 = new $.ScrollMagic.Scene({
+    triggerElement: '#skills ul li',
+}).setClassToggle('.hidden', 'fadein').addTo(controller);
---
+++ scripts/variables.js
@@ -0,0 +1,134 @@
+let english = {
+    header:{
+        listItems: ['Home', 'About', 'Projects', 'Contact', 'Icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>']
+    },
+    main:{
+        mainTitle: 'Web Development Student',
+    },
+    about:{
+        title: 'My name is Patrick Minero, a customer service professional learning how to code.',
+        paragraph1: 'After college, I emigrated to California in the hopes of finding better job opportunities. However, due to the 2008 recession, it was difficult to find stable jobs and I started to look for positions that would allow me to grow on the corporate ladder. I found a dishwashing job at a local bakery chain and within 3 years I was promoted to management.',
+        paragraph2: 'Through this experience, I gained valuable work skills such as team leadership and management, payroll and labor management, sales projections and financial management. These skills have helped me in my personal and professional endeavors for many years. However, after some time, I felt the need to change my life and acquire profitable skills that would allow me to have a more fulfilling and stable work life.',
+        paragraph3: 'In 2018, I left California and embarked on a one year trip volunteering in family farms around the world. This experience helped me grow as a person and encouraged me to make the decision to do something about my career.',
+        paragraph4: 'I cut short my trip to go back home and help my family’s struggling business and in my spare time I started learning the basics of web development to see if I liked it as a career choice. I came across <span>Codecademy</span>, which is a great tool for complete beginners and after a couple of weeks I was hooked.',
+        paragraph5: 'Then I moved to beautiful Barcelona, where I made more time to delve into my new found interest. During this time, I found great tools and resources online that helped me built projects using:',
+        journey: 'My Programming Journey',
+    },
+    projects:{
+        title: 'Projects',
+        projectOne: {
+            title: 'Weather Web App',
+            image: 'media/weather-app.jpg',
+            alt: 'Weather app thumbnail',
+            url: 'https://patrickminero.github.io/weather_app/',
+            description: 'Responsive weather and web application. Created using 3 different API\'s and styled with Sass.',
+        },
+        projectTwo: {
+            title: 'To-do Web App',
+            image: 'media/to-do-app.jpg',
+            alt: 'To do app thumbnael',
+            url:'https://patrickminero.github.io/updated_projects/to-do-app/',
+            description: 'Simple, responsive and efficient to-do list web application created with Vue.js, Sass',
+        },
+        projectThree: {
+            title: 'Responsive Gallery',
+            url: '#',
+            image: 'media/image-gallery.jpg',
+            alt: 'Image gallery thumbnail',
+            description: 'Image gallery with filter, responsive and efficent dense layout with lightbox full screen view.',
+        }
+    },
+    references:{
+        title: 'Employer Feedback',
+        initial:'As the person in charge of the cafe, Patrick demonstrated responsibility, dedication and assertiveness. His leadership and commitment...',
+        equator: 'As the person in charge of the cafe, Patrick demonstrated responsibility, dedication and assertiveness. His leadership and commitment to the company helped establish our clientele on what would become the first step on our very successful retail cafe venture.',
+        beerworks: 'Personally, Patrick is thoughtful, humble, gracious and has endless integrity. Professionally, Patrick has a fierce work ethic and is tremendously self-motivated. From my observation, he thrives on being productive and executing his job at a high level. He is ultra sharp and always eager to accumulate more knowledge. Patrick leads by example and has always set the tone for his peers and helped establish the culture in our restaurants. Furthermore, his passion for food and beverage really shines through in his engagement with customers, further elevating their experience.',
+    },
+    contact:{
+        title: 'Contact me',
+        name:'Your Name...',
+        email: 'Your Email...',
+        message: 'Message...',
+        submit: 'Send Message',
+        footer: {
+            h2: 'Written with love in Barcelona',
+            h3: 'Flag icons made by <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>'
+        }
+    },
+};
+let spanish = {
+    header:{
+        listItems: ['Inicio', 'Sobre mí', 'Proyectos', 'Contacto', 'Íconos creados por <a href="https://www.flaticon.com/authors/roundicons" title="Roundicons">Roundicons</a> en <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>']
+    },
+    main:{
+        mainTitle: 'Estudiante de Desarrollo Web',
+    },
+    about:{
+        title: 'Mi nombre es Patrick Minero, un profesional del servicio al cliente aprendiendo a programar.',
+        paragraph1: 'After college, I emigrated to California in the hopes of finding better job opportunities. However, due to the 2008 recession, it was difficult to find stable jobs and I started to look for positions that would allow me to grow on the corporate ladder. I found a dishwashing job at a local bakery chain and within 3 years I was promoted to management.',
+        paragraph2: 'Through this experience, I gained valuable work skills such as team leadership and management, payroll and labor management, sales projections and financial management. These skills have helped me in my personal and professional endeavors for many years. However, after some time, I felt the need to change my life and acquire profitable skills that would allow me to have a more fulfilling and stable work life.',
+        paragraph3: 'In 2018, I left California and embarked on a one year trip volunteering in family farms around the world. This experience helped me grow as a person and encouraged me to make the decision to do something about my career.',
+        paragraph4: 'I cut short my trip to go back home and help my family’s struggling business and in my spare time I started learning the basics of web development to see if I liked it as a career choice. I came across <span>Codecademy</span>, which is a great tool for complete beginners and after a couple of weeks I was hooked.',
+        paragraph5: 'Then I moved to beautiful Barcelona, where I made more time to delve into my new found interest. During this time, I found great tools and resources online which helped further support what I was learning on Codecademy and now I have experience with <span>HTML5, Bootstrap, Materialize, CSS3, Sass, JavaScript, Jquery, Vue.js, GIT, CLI</span>. I am always excited to learn more skills.',
+    },
+    projects:{
+        title: 'Proyectos',
+        projectOne: {
+            title: 'Aplicación Web del Clima',
+            image: 'media/weather-app.jpg',
+            alt: 'Viñeta de la aplicación del clima',
+            url: 'https://patrickminero.github.io/weather_app/',
+            description: 'Aplicación escalable del clima actual y de pronóstico de 5 días. Creada usando Open Weather, Google places y locationIQ API, diseñada con Sass.',
+        },
+        projectTwo: {
+            title: 'Aplicación Web de tareas',
+            image: 'media/to-do-app.jpg',
+            alt: 'Viñeta de applicación de tareas',
+            url:'https://patrickminero.github.io/updated_projects/to-do-app/',
+            description: 'Simple, escalable y eficiente aplicación web de tareas creada con Vue.js, Sass y local storage',
+        },
+        projectThree: {
+            title: 'Gallería Escalable',
+            url: '#',
+            image: 'media/image-gallery.jpg',
+            alt: 'Viñeta de gallería de imágenes',
+            description: 'Increíble manera de publicar multiples imágenes, escalable y eficiente diseño denso.',
+        }
+    },
+    references:{
+        title: 'Lo que dicen las empresas',
+        equator: 'As the person in charge of the cafe, Patrick demonstrated responsibility, dedication and assertiveness. His leadership and commitment to the company helped establish our clientele on what would become the first step on our very successful retail cafe venture.',
+        beerworks: 'Personally, Patrick is thoughtful, humble, gracious and has endless integrity. Professionally, Patrick has a fierce work ethic and is tremendously self-motivated. From my observation, he thrives on being productive and executing his job at a high level. He is ultra sharp and always eager to accumulate more knowledge. Patrick leads by example and has always set the tone for his peers and helped establish the culture in our restaurants. Furthermore, his passion for food and beverage really shines through in his engagement with customers, further elevating their experience.',
+    },
+    contact:{
+        title: 'Contáctame',
+        name:'Tu nombre...',
+        email: 'Tu Email...',
+        message: 'Mensaje...',
+        submit: 'Enviar Mensaje',
+        footer: {
+            h3: 'Escrito con amor en Barcelona',
+        }
+    },
+};
+
+// let menu = document.querySelector('#menu-button');
+// let  navBar = document.querySelector('#nav-bar');
+// let  englishButton= document.querySelector('#english');
+// let spanishButton = document.querySelector('#spanish');
+// let  homeButton = document.querySelector('#home-button');
+// let  aboutButton = document.querySelector('#about-button');
+// let  projectsButton = document.querySelector('#projects-button');
+// let  contactButton = document.querySelector('#contact-button');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
+// let  = document.querySelector('#');
